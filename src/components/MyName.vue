d5828cf59a3acef35d9d923a1d964442350565ae
rrac0008 <rrac0008@student.monash.edu>
2025-08-12 19:49:50 +1000

    Initial commit of Vue project

---
+++ src/components/MyName.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="my_name">
+    <h2>I am Rachit Rajiv</h2>
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'MyName',
+
+};
+</script>
+
+<style scoped>
+.my_name {
+  font-size: 24px;
+  color: #3eaf7c;
+  margin-top: 20px;
+}
+</style>
