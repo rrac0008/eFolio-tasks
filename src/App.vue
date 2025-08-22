37c2416d4cadf8fb724a1f6351452fc00685958c
rrac0008 <rrac0008@student.monash.edu>
2025-08-22 16:12:28 +1000

    Style forms using css

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,24 @@
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 import JSON from './components/JSON.vue'
+import Form from './components/Form.vue'
 </script>
 
 <template>
-  <header>
+  <!--header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
     </div>
-  </header>
+  </header-->
 
   <main>
-    <JSON />
+    <!--JSON /-->
+    <Form />
   </main>
 </template>
+
 
 <style scoped>
 header {
